1a73a9072d7500bd8e342386ebadecaa12f5e23a
Vitor Caetano Teixeira <vitor130595@gmail.com>
2019-06-19 09:51:22 -0300

    FirstCommit

---
+++ TesteViajaNet/TesteViajaNet/Controllers/ColetaDadosController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ColetaDadosController : Controller
+    {
+        // POST: ColetaDados
+        // Envia os dados de navegação para o Banco de Dados
+        [HttpPost]
+        public ActionResult Index(ParametroColetado dados)
+        {
+
+
+
+            return new HttpStatusCodeResult(200); 
+        }
+    }
+}
---
+++ TesteViajaNet/TesteViajaNet/Controllers/ParametroColetado.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication1.Controllers
+{
+    public class ParametroColetado
+    {
+
+        public string ip { get; set; }
+        public string nomePagina { get; set; }
+        public string browser { get; set; } 
+        public string parametrosPagina {get; set;}
+
+    }
+}
